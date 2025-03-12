ddf78078743029900e20dd1060d0ab872f2ab15c
nhuy <nguyenngocnhuy456@gmail.com>
2025-03-12 20:43:53 +0700

    delete comment

--- database/migrations/2025_03_07_142601_creat_rankings.php
+++ database/migrations/2025_03_07_142601_creat_rankings.php
@@ -14,16 +14,11 @@ return new class extends Migration
     {
         Schema::create('rankings', function (Blueprint $table) {
             $table->id();
-            // user_id , total_score, period, start_date, end_date, created_at, updated_at
             $table->unsignedBigInteger('user_id')->nullable();
             $table->decimal('total_score', 15, 2)->nullable();
             $table->integer('period')->nullable();
             $table->date('start_date')->nullable();
             $table->date('end_date')->nullable();
-            // thời gian tạo bảng xếp hạng
-           // $table->timestamp('created_at')->useCurrent();
-            // thời gian cập nhật bảng xếp hạng
-            // $table->timestamp('updated_at')->useCurrent();
             $table->timestamps();
         });
     }
--- database/migrations/2025_03_07_144631_create_api_logs.php
+++ database/migrations/2025_03_07_144631_create_api_logs.php
@@ -13,10 +13,8 @@ return new class extends Migration
     {
         Schema::create('api_logs', function (Blueprint $table) {
             $table->id();
-            // api_endpoint, request_body, response_body, status_code, submission_id
-            $table->unsignedBigInteger('submission_id')->nullable();
-            // e+api_endpoint có kiểu dữ liệu varchar
-            $table->string('api_endpoint')->nullable();
+             $table->unsignedBigInteger('submission_id')->nullable();
+             $table->string('api_endpoint')->nullable();
             $table->text('request_body')->nullable();
             $table->text('response_body')->nullable();
             $table->integer('status_code')->nullable();
