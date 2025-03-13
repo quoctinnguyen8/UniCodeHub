150c73db04251458c4b0e1cdb485eb74d26c5608
vokhang9999 <anhao11803@gmail.com>
2025-03-13 10:50:45 +0700

    hoàn thành code

---
+++ database/migrations/2025_03_13_034348_create_exercises2s_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('exercises2s', function (Blueprint $table) {
+            $table->id(); // ID tự động tăng
+            $table->string('title', 255)->nullable(); // Tiêu đề bài tập (có thể NULL)
+            $table->text('description')->nullable(); // Mô tả bài tập (có thể NULL) 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('exercises2s');
+    }
+};
