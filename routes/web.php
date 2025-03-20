2b07db72a4086cf896ed97e2820f4b5b57441af2
TINH\phamt <phamtinh9099@gmail.com>
2025-03-20 07:40:40 +0700

    [0010] Quản lý thông tin tài khoản: Hiển thị danh sách

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::get('/users', [AdminController::class, 'index'])->name('users.index');
 });
